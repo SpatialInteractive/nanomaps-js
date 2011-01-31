5c49eea56f94ea371a8d818219b3f16128a0bf55
Stella Laurenzo <stella@laurenzo.org>
2011-01-31 12:59:50 -0800

    Refactor source files

---
+++ client/lib/nanomaps.bundle.all.js
@@ -0,0 +1,15 @@
+##EJSON
+/*!
+ * Nanomaps JavaScript Library
+ * https://github.com/stellaeof/nanomaps
+ * Copyright 2011, Stella Laurenzo
+ * Licensed under the MIT open source license
+ * Date: #{new Date()}
+ */
+##include('components/nanomaps.core.js')
+##include('components/nanomaps.tiles.js')
+##include('components/nanomaps.desktop.js')
+##include('components/nanomaps.touch.js')
+##include('components/nanomaps.imgmarker.js')
+
+
