d498127510dad669e391ba230db4d78f5c81531e
Stella Laurenzo <stella@laurenzo.org>
2011-01-31 16:47:12 -0800

    Add standard image markers

--- client/lib/components/nanomaps.imgmarker.js
+++ client/lib/components/nanomaps.imgmarker.js
@@ -5,22 +5,74 @@
  */
 (function(nanomaps) {
 
+function makeAbsolute(baseUri, href) {
+	if (href[0]==='/' || href.search(/^http(s)?\:/)>=0) return href;
+	if (baseUri.match(/\/$/)) return baseUri+href;
+	else return baseUri+'/'+href;
+}
+
+function hrefToClassSuffix(href) {
+	var i=href.lastIndexOf('/'),
+		baseName=(i>=0 ? href.substring(i+1) : href),
+		m=baseName.match(/^([^_\-\.]+)/);
+	return m ? m[0] : href;
+}
+
 /**
  * Primordial image marker class.  Image markers are managed according
  * to the prototype pattern.  If you have one, you can customize it into
  * a new one.  Any marker instance can be used in a call to MapSurface.attach
  * due to it fulfilling the element factory interface.
+ * @public
  */
-function ImgMarker(options) {
+function ImgMarker(settings) {
+	this.settings=new SettingsConstructor(settings);
 }
+/**
+ * Global settings inherited for each instance
+ */
+ImgMarker.Settings={
+	/**
+	 * The img src.  If primarySuffix is not specified,
+	 * then the first alpha-numeric part of this string
+	 * is taken.
+	 */
+	src: 'orb_blue.png',
+	
+	/**
+	 * The uri to resolve relative sources against
+	 */
+	baseUri: '',
+	
+	/**
+	 * Latitude to set at creation time
+	 */
+	latitude: 0,
+	
+	/**
+	 * Longitude to set at creation time
+	 */
+	longitude: 0,
+	
+	/**
+	 * If set, then this is the primary css suffix.  If not set,
+	 * it is derived from the src.
+	 */
+	 classSuffix: '',
+	 
+	 /**
+	  * Extra css classes
+	  */
+	 extraClasses: ''
+};
+
 ImgMarker.prototype={
 	/**
 	 * Fulfill the factory pattern for map attachments.
 	 * All img markers have the following structure:
 	 * <pre>
-	 * <div class="nmim {alignment classes}">
-	 *   <img src="{src url}" width="{width}" height="{height}" class="nmim-fg"/>
-	 *   <img src="{background url}" ... class="nmim-bg" />
+	 * <div class="nmim">
+	 *   <img src="{src url}" class="nmim-fg {src class}"/>
 	 * </div>
 	 * </pre>
 	 * A background will only be specified if setup in the properties.  In addition,
@@ -32,28 +84,44 @@ ImgMarker.prototype={
 			settings=this.settings,
 			outerElt=d.createElement('div'),
 			fgElt=d.createElement('img'),
-			geo={};
+			fgSrc,
+			classSuffix;
 			
-		outerElt.className='nmim';
-		outerElt.appendChild(fgElt);
-		outerElt.geo=geo;
-		
+			
 		// Configure fg img elt
+		fgSrc=makeAbsolute(settings.baseUri||'', settings.src||'');
+		classSuffix=settings.classSuffix || hrefToClassSuffix(fgSrc);
+		
+		outerElt.className='nmim nmim-' + classSuffix + ' ' + settings.extraClasses;
+		outerElt.setAttribute('latitude', settings.latitude||0);
+		outerElt.setAttribute('longitude', settings.longitude||0);
+		outerElt.appendChild(fgElt);
+		
 		fgElt.className='nmim-fg';
-		fgElt.setAttribute('width', _width(settings));
-		fgElt.setAttribute('height', _height(settings));
-		fgElt.setAttribute('src', _src(settings));
-		
-		
-		// Set geo properties
-		geo.latitude=_latitude(settings);
-		geo.longitude=_longitude(settings);
-		geo.xoffset=_alignXOffset(settings);
-		geo.yoffset=_alignYOffset(settings);
+		fgElt.setAttribute('src', fgSrc);
 		
 		return outerElt;
 	}
 };
-	
+
+/**
+ * Used internally to create a new settings object with the global
+ * settings as its prototype.
+ * @private
+ */
+function SettingsConstructor(override) {
+	if (override) {
+		for (var k in override) {
+			if (override.hasOwnProperty(k))
+				this[k]=override[k];
+		}
+	}
+}
+SettingsConstructor.prototype=ImgMarker.Settings;
+
+
+// Exports
+nanomaps.ImgMarker=ImgMarker;
+
 })(nanomaps);
 
